29cd272b2b2c7c2ec6e6080bbad094277ae2ff4d
Star <108232389+Star-Dude@users.noreply.github.com>
2024-09-13 02:52:45 +0200

    Oppdaterte O2.java til å være rett
    Originale koden var basert på en misforståelse av skattesystemet i Norge, og denne 'forbedrede' koden fungerer til å finne korrekt skattemengde rundet ned. MEN, dette er kanskje en av de verste programmene jeg har skrevet med noen veldig interressante valg, skal ikke lyve selv. Er 100% bedre måter å gjøre dette på, men ingen måter som jeg kommer til å bry meg om å finne ut av nå. Lykke til til den som må lese gjennom og forstå dette.

--- DAT100Innlevering1/src/no/hvl/dat100/O2.java
+++ DAT100Innlevering1/src/no/hvl/dat100/O2.java
@@ -13,39 +13,69 @@ public class O2 {
 
         listener.close();
 
-        double tax = 0;
+        double taxDecimal = 0;
+        double paidTax = 0;
 
         String bracket = null;
 
-        if (income >= 208051 && income <= 292850) {
-            tax = 1.7;
+        double[] bracket1 = {208051, 292850};
+        double[] bracket2 = {bracket1[1] + 1, 670000};
+        double[] bracket3 = {bracket2[1] + 1, 937900};
+        double[] bracket4 = {bracket3[1] + 1, 1350000};
+        double[] bracket5 = {bracket4[1] + 1};
+
+        double maxTax1 = (bracket1[1] - bracket1[0]) * 1.7 / 100;
+        double maxTax2 = (bracket2[1] - bracket2[0]) * 4.0 / 100 + maxTax1;
+        double maxTax3 = (bracket3[1] - bracket3[0]) * 13.6 / 100 + maxTax2;
+        double maxTax4 = (bracket4[1] - bracket4[0]) * 16.6 / 100 + maxTax3;
+        
+        if (income < bracket1[1] && income > 208050) {
+            taxDecimal = 1.7 / 100;
+            paidTax = (income - bracket1[0] - 1) * taxDecimal;
             bracket = "trinn 1";
         }
-        else if (income >= 292851 && income <= 670000) {
-            tax = 4.0;
+        else if (income < bracket2[1]) {
+            taxDecimal = 4.0 / 100;
+            paidTax = (income - bracket2[0] - 1) * taxDecimal;
             bracket = "trinn 2";
         }
-        else if (income >= 670001 && income <= 937900) {
-            tax = 13.6;
+        else if (income < bracket3[1]) {
+            taxDecimal = 13.6 / 100;
+            paidTax = (income - bracket3[0] - 1) * taxDecimal;
             bracket = "trinn 3";
         }
-        else if (income >= 937901 && income <= 1350000) {
-            tax = 16.6;
+        else if (income < bracket4[1]) {
+            taxDecimal = 16.6 / 100;
+            paidTax = (income - bracket4[0] - 1) * taxDecimal;
             bracket = "trinn 4";
         }
-        else if (income >= 1350001) {
-            tax = 17.6;
+        else if (income < bracket5[1]) {
+            taxDecimal = 17.6 / 100;
+            paidTax = (income - bracket5[0] - 1) * taxDecimal;
             bracket = "trinn 5";
         }
-
-        double taxDecimal = tax / 100;
-        double paidTax = income * taxDecimal;
         
         if (bracket == null) {
             System.out.println("Med en inntekt på under 208 051 kroner i året, betaler du ikke skatt. Hurra!");
         }
         else {
-            System.out.printf("Med en inntekt på %dkr, ligger du på %s med %.1f%% i trinnskatt, og du må betale %.2fkr i skatt.", income, bracket, tax, paidTax);
+            switch (bracket) {
+                case "trinn 2":
+                    paidTax += maxTax1;
+                    break;
+                case "trinn 3":
+                    paidTax += maxTax2;
+                    break;
+                case "trinn 4":
+                    paidTax += maxTax3;
+                    break;
+                case "trinn 5":
+                    paidTax += maxTax4;
+                    break;
+                default:
+            }
+
+            System.out.printf("Med en inntekt på %dkr, ligger du på %s med %.1f%% i trinnskatt, og du må betale %.2fkr i skatt.", income, bracket, taxDecimal*100, paidTax);
         }
     }
 
