b80200bfd5c191b249e078d533c42826123f5c7b
Star <108232389+Star-Dude@users.noreply.github.com>
2024-09-13 22:31:13 +0200

    Liten bugfix oppdatering
    Fortsatt forferdelig kode, men litt mindre forferdelig.

--- DAT100Innlevering1/src/no/hvl/dat100/O2.java
+++ DAT100Innlevering1/src/no/hvl/dat100/O2.java
@@ -9,7 +9,7 @@ public class O2 {
     public static void calculateBracketTax() {
 
         System.out.print("Skriv inn inntekt: \n > ");
-        int income = listener.nextInt();
+        double income = listener.nextDouble();
 
         listener.close();
 
@@ -22,37 +22,38 @@ public class O2 {
         double[] bracket2 = {bracket1[1] + 1, 670000};
         double[] bracket3 = {bracket2[1] + 1, 937900};
         double[] bracket4 = {bracket3[1] + 1, 1350000};
-        double[] bracket5 = {bracket4[1] + 1};
 
         double maxTax1 = (bracket1[1] - bracket1[0]) * 1.7 / 100;
         double maxTax2 = (bracket2[1] - bracket2[0]) * 4.0 / 100 + maxTax1;
         double maxTax3 = (bracket3[1] - bracket3[0]) * 13.6 / 100 + maxTax2;
         double maxTax4 = (bracket4[1] - bracket4[0]) * 16.6 / 100 + maxTax3;
         
-        if (income < bracket1[1] && income > 208050) {
-            taxDecimal = 1.7 / 100;
-            paidTax = (income - bracket1[0] - 1) * taxDecimal;
-            bracket = "trinn 1";
-        }
-        else if (income < bracket2[1]) {
-            taxDecimal = 4.0 / 100;
-            paidTax = (income - bracket2[0] - 1) * taxDecimal;
-            bracket = "trinn 2";
-        }
-        else if (income < bracket3[1]) {
-            taxDecimal = 13.6 / 100;
-            paidTax = (income - bracket3[0] - 1) * taxDecimal;
-            bracket = "trinn 3";
-        }
-        else if (income < bracket4[1]) {
-            taxDecimal = 16.6 / 100;
-            paidTax = (income - bracket4[0] - 1) * taxDecimal;
-            bracket = "trinn 4";
-        }
-        else if (income < bracket5[1]) {
-            taxDecimal = 17.6 / 100;
-            paidTax = (income - bracket5[0] - 1) * taxDecimal;
-            bracket = "trinn 5";
+        if (income > bracket1[0]) {
+            if (income <= bracket1[1]) {
+                taxDecimal = 1.7 / 100;
+                paidTax = (income - bracket1[0] - 1) * taxDecimal;
+                bracket = "trinn 1";
+            }
+            else if (income <= bracket2[1]) {
+                taxDecimal = 4.0 / 100;
+                paidTax = (income - bracket2[0] - 1) * taxDecimal;
+                bracket = "trinn 2";
+            }
+            else if (income <= bracket3[1]) {
+                taxDecimal = 13.6 / 100;
+                paidTax = (income - bracket3[0] - 1) * taxDecimal;
+                bracket = "trinn 3";
+            }
+            else if (income <= bracket4[1]) {
+                taxDecimal = 16.6 / 100;
+                paidTax = (income - bracket4[0] - 1) * taxDecimal;
+                bracket = "trinn 4";
+            }
+            else if (income > bracket4[1]) {
+                taxDecimal = 17.6 / 100;
+                paidTax = (income - bracket4[1] + 1) * taxDecimal;
+                bracket = "trinn 5";
+            }
         }
         
         if (bracket == null) {
@@ -75,7 +76,7 @@ public class O2 {
                 default:
             }
 
-            System.out.printf("Med en inntekt på %dkr, ligger du på %s med %.1f%% i trinnskatt, og du må betale %.2fkr i skatt.", income, bracket, taxDecimal*100, paidTax);
+            System.out.printf("Med en inntekt på %.2fkr, ligger du på %s med %.1f%% i trinnskatt, og du må betale %.2fkr i skatt.", income, bracket, taxDecimal*100, paidTax);
         }
     }
 
